388af86649ea77e0d4aa60b9f7ded7d83d69ec0d
Cletus Caido <cletuscaido@gmail.com>
2026-02-05 15:43:00 +0800

    Merge alembic heads

---
+++ migrations/versions/e749b01e5ffd_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: e749b01e5ffd
+Revises: 485ffa97c53d, f2e1c1234abc
+Create Date: 2026-02-05 15:42:11.188525
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e749b01e5ffd'
+down_revision = ('485ffa97c53d', 'f2e1c1234abc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
